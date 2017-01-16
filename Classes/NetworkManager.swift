be910d7ed2dc577c9532a33af422c0405f8fedfd
Ayelen Chavez <ayelen.chavez@olx.com>
2017-01-16 16:18:15 +0100

    gitignore

---
+++ Classes/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  Example
+//
+//  Created by Ayelen Chavez on 16/01/2017.
+//  Copyright © 2017 Ayelen Chavez. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager: NSObject {
+
+}
