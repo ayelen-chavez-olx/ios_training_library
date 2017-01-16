1b9f4ec9849125da648c754333f4421d8a19e943
Ayelen Chavez <ayelen.chavez@olx.com>
2017-01-16 16:19:27 +0100

    first commit

--- Classes/NetworkManager.swift
+++ Classes/NetworkManager.swift
@@ -7,7 +7,55 @@
 //
 
 import UIKit
+import Foundation
 
-class NetworkManager: NSObject {
+public class App {
+    let appleID : String
+    let bundleID : String
+    let iconURL : String
+    let name : String
+    let APPLE_ID_KEY = "AppleID"
+    let BUNDLE_ID_KEY = "BundleID"
+    let ICON_URL_KEY = "IconURL"
+    let NAME_KEY = "Name"
+    
+    init(dict: Dictionary<String, Any>) {
+        appleID = dict[APPLE_ID_KEY] as? String ?? ""
+        bundleID = dict[BUNDLE_ID_KEY] as? String ?? ""
+        iconURL = dict[ICON_URL_KEY] as? String ?? ""
+        name = dict[NAME_KEY] as? String ?? ""
+    }
+}
 
+public class NetworkManager {
+    let url = "https://www.moonlightapps.com/appnetwork/appnetwork.svc/en/data"
+    
+    public static let instance = NetworkManager()
+    
+    private init() {
+    }
+
+    public func downloadContent(completionHandler:@escaping ([App]?)->()) {
+        let task = URLSession.shared.dataTask(with: URL(string:url)!) { (data, response, error) in
+            var apps = [App]()
+            
+            if (error == nil)  {
+                if let myData = data {
+                    do {
+                        if let appsDefinition = try JSONSerialization.jsonObject(with: myData, options: .allowFragments) as? [Dictionary<String, Any>] {
+                            for appDict in appsDefinition {
+                                apps.append(App(dict: appDict))
+                            }
+                        }
+                    } catch {
+                        print("Error trying to parse data")
+                    }
+                }
+            }
+            
+            completionHandler(apps)
+        }
+        
+        task.resume()
+    }
 }
---
+++ Example/Pods/Target Support Files/ios_training_library/ios_training_library-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ios_training_library : NSObject
+@end
+@implementation PodsDummy_ios_training_library
+@end
